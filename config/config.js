ee3e7573eace548d689725137dc6052129450a63
rzkimaulana <jambi1467@gmail.com>
2024-12-20 15:18:34 +0700

    fix

--- config/config.js
+++ config/config.js
@@ -1,34 +1,31 @@
-require("dotenv").config();
-
+require('dotenv').config();
 const config = {
   development: {
-    username: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
-    host: process.env.PGHOST,
-    dialect: 'postgres',
+    username:process.env.PGUSER,
+    password:process.env.PGPASSWORD,
+    database:process.env.PGDATABASE,
+    host:process.env.PGHOST,
+    dialect:"postgres",
     dialectOptions: {
       ssl: {
-        require: true,
-        rejectUnauthorized: false
+        require:true,
+        rejectUnauthorized:false
       }
     }
-
   },
   production: {
-    username: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
-    host: process.env.PGHOST,
-    dialect: 'postgres',
+    username:process.env.PGUSER,
+    password:process.env.PGPASSWORD,
+    database:process.env.PGDATABASE,
+    host:process.env.PGHOST,
+    dialect:"postgres",
     dialectOptions: {
       ssl: {
-        require: true,
-        rejectUnauthorized: false
+        require:true,
+        rejectUnauthorized:false
       }
     }
-
   },
-}
+};
 
 module.exports = config;
