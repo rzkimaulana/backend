4da7c7aa4975836cbe6845e568374b971c8dd01d
rzkimaulana <jambi1467@gmail.com>
2024-12-20 15:28:38 +0700

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const router = require("./routes");
+const router = require("./src/routes");
 
 const app = express();
 const port = process.env.PORT || 3002;
