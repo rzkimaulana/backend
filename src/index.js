89254c9165d650acf3e90d8f3c828b1f7579e7fb
rzkimaulana <jambi1467@gmail.com>
2024-12-20 15:05:30 +0700

    fix commit

--- src/index.js
+++ src/index.js
@@ -1,29 +1,26 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require('body-parser')
-const routers = require("./routes")
+
+const router = require("./routes");
 
 const app = express();
-const port = process.env.PORT || 3002
+const port = process.env.PORT || 3002;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-// Routes
-app.use(routers);
 
+// Routes
+app.use(router);
+
+// Rute untuk testing
 app.get('/', (req, res) => {
   res.send({
     message: 'Hello 👋',
     status: 'Server ready 🚀',
   });
 });
-app.listen(port, () => {
-  console.log(`Server ready listening on http://localhost:${port}`)
-})
 
-// Export aplikasi Express sebagai modul
+
 module.exports = app;
