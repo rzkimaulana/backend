61fcc898dd0ba32d55b00a7ecdbbfcf508543fa7
rzkimaulana <jambi1467@gmail.com>
2024-12-20 15:46:11 +0700

    fix

--- api/routes/userroutes.js
+++ api/routes/userroutes.js
@@ -1,9 +1,9 @@
-const { loginUser, buatuser } = require("../controller/usercontroller")
-
 const express = require("express");
+const { loginUser, buatuser } = require("../controller/usercontroller");
+
 const router = express.Router();
 
 router.post("/register", buatuser);
 router.post("/login", loginUser);
 
-module.exports = router;
+module.exports = router; 
