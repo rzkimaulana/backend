b6eabb61a1d4bbadc9d1cd9281b66565d7c20379
rzkimaulana <jambi1467@gmail.com>
2024-12-20 14:10:00 +0700

    update lagi be

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const bodyParser = require('body-parser')
 // Import routes
 const userRoutes = require("../api/routes/userroutes");
 const siswaRoutes = require("../api/routes/siswaroutes");
@@ -11,7 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 // Routes
 app.use('/api/user', userRoutes);
 app.use('/api/siswa', siswaRoutes);
